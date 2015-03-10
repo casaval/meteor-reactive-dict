b99a6b0a20e63bb3709a9a95e624764d248d331e
joseme08 <joseme08@noa.nintendo.com>
2015-03-10 16:08:44 -0700

    initial commit

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  summary: "Reactive dictionary",
+  version: '1.0.6-winr.3'
+});
+
+Package.onUse(function (api) {
+  api.use(['underscore', 'tracker', 'ejson']);
+  // If we are loading mongo-livedata, let you store ObjectIDs in it.
+  api.use('mongo', {weak: true});
+  api.export('ReactiveDict');
+  api.addFiles('reactive-dict.js');
+  api.addFiles('migration.js');
+});
+
+Package.onTest(function (api) {
+  api.use('tinytest');
+});
